16ed411d1db5fdd1a89ced97ca4379effba3d2d8
mdhmz1 <hamzah.mh1@gmail.com>
2020-10-11 00:13:40 +0530

    Added segmented annotation support

---
+++ annotate.py
@@ -0,0 +1,212 @@
+import numpy as np
+from skimage.measure import find_contours
+from shapely.geometry import Polygon, MultiPolygon 
+from skimage import measure   
+import json
+import os
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from mrcnn.visualize import display_instances
+from mrcnn.config import Config
+from mrcnn.model import MaskRCNN
+import matplotlib.pyplot as plt
+import tensorflow as tf
+tf.logging.set_verbosity(tf.logging.ERROR)
+
+
+def annotateResult(result, image_name, label):
+    n = len(result['class_ids'])
+    annotations = []
+    annotationId = 1
+    for i in range(n):
+        if class_names[result['class_ids'][i]] == label:
+            annotation = create_sub_mask_annotation(result['masks'][:, :, i], result['rois'][i],
+                                                    annotationId, result['class_ids'][i], image_name)
+            annotations.append(annotation)
+            annotationId += 1
+    return annotations
+
+
+def create_sub_mask_annotation(sub_mask, bounding_box, annotationId, classId, image_name):
+    # Find contours (boundary lines) around each sub-mask
+    contours = measure.find_contours(sub_mask, 0.5, positive_orientation='low')
+
+    segmentations = []
+    polygons = []
+    for contour in contours:
+        # Flip from (row, col) representation to (x, y)
+        # and subtract the padding pixel
+        for i in range(len(contour)):
+            row, col = contour[i]
+            contour[i] = (col - 1, row - 1)
+
+        # Make a polygon and simplify it
+        poly = Polygon(contour)
+        poly = poly.simplify(1.0, preserve_topology=False)
+        polygons.append(poly)
+        segmentation = np.array(poly.exterior.coords).ravel().tolist()
+        segmentations.append(segmentation)
+
+    # Combine the polygons to calculate the bounding box and area
+    multi_poly = MultiPolygon(polygons)
+    x, y, max_x, max_y = multi_poly.bounds
+    width = max_x - x
+    height = max_y - y
+    bbox = (x, y, width, height)
+    # area = multi_poly.area
+
+    annotation = {
+        'filename': image_name,
+        'id': annotationId,
+        'label': str(class_names[classId]),
+        'bbox': bbox,
+        'segmentation': segmentations,
+    }
+
+    return annotation
+
+
+
+def writeToJSONFile(path, fileName, data):
+    fileName = fileName.split(".")[0]
+    filePathNameWExt =  path + '/' + fileName + '.json'
+    with open(filePathNameWExt, 'w') as fp:
+        json.dump(data, fp)
+
+
+def annotateAndSaveAnnotations(r, directory, image_name, label):
+    annotationsJson = annotateResult(r, image_name, label)
+    writeToJSONFile(directory, image_name, annotationsJson)
+
+
+def annotateImagesInDirectory(rcnn, directory_path, label):
+    for fileName in os.listdir(directory_path):
+        if fileName.endswith(".jpg") or fileName.endswith(".jpeg") or fileName.endswith(".png"):
+        # load image
+            print("Evaluating Image: " + fileName)
+            img = load_img(directory_path+"/"+fileName)
+            img = img_to_array(img)
+            # make prediction
+            results = rcnn.detect([img], verbose=0)
+            # get dictionary for first prediction
+            result = results[0]
+
+            if class_names.index(label) in result['class_ids']:
+                print("Label found in image: " + fileName)
+                print("Annotating...")
+                annotateAndSaveAnnotations(result, directory_path, fileName, label)
+                if (args.displayMaskedImages is True):
+                    display_instances(img, result['rois'], result['masks'], result['class_ids'],
+                                  class_names, class_names.index(label), result['scores'])
+            else:
+                print("Label not found in image: " + fileName)
+
+
+ROOT_DIR = os.path.abspath("./")
+COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "./weight/mask_rcnn_coco.h5")
+
+# Directory to save logs, if not provided
+# through the command line argument --logs
+DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+
+if __name__ == '__main__':
+    import argparse
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description = 'Annotate the object')
+    parser.add_argument("command",
+                        metavar="<command>",
+                        help="'annotateCoco' or 'annotateCustom'")
+    parser.add_argument('--image_directory', required=True,
+                        metavar="/path/to/the/image/directory/",
+                        help='Directory of the images that need to be annotated')
+    parser.add_argument('--weights', required=True,
+                        metavar="/path/to/weights.h5",
+                        help="path_to_weights.h5_file or 'coco_weights'")
+    parser.add_argument('--logs', required=False,
+                        default=DEFAULT_LOGS_DIR,
+                        metavar="/path/to/logs/",
+                        help='Logs and checkpoints directory (default=logs/)')
+    parser.add_argument('--label', required=True,
+                        metavar="object_label_to_annotate",
+                        help='Either COCO dataset labels or custom')
+    parser.add_argument('--displayMaskedImages', type=bool,
+                        default=False, required=False,
+                        help='Display the masked images.')
+                        
+    args = parser.parse_args()
+
+    # Validate arguments
+    if args.command == "annotateCoco":
+        assert args.label in class_names, "Label --label does not belong to COCO labels "
+
+    elif args.command == "annotateCustom":
+        assert args.label, "Argument --label is required for annotation"
+
+    assert args.image_directory, "Argument --image_directory is required for annotation"
+    assert args.weights, "Argument --weights is required for annotation"
+
+
+    class InferenceCocoConfig(Config):
+        # Set batch size to 1 since we'll be running inference on
+        # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+        NAME = "inferenceCoco"
+        GPU_COUNT = 1
+        IMAGES_PER_GPU = 1
+        NUM_CLASSES = 1 + 80
+        
+    class InferenceCustomConfig(Config):
+        NAME = "inferenceCustom"
+        GPU_COUNT = 1
+        IMAGES_PER_GPU = 1
+        NUM_CLASSES = 1 + 1
+
+
+    if args.command == "annotateCoco":
+        config = InferenceCocoConfig()
+        class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
+    else:
+        config = InferenceCustomConfig()
+        class_names = ['BG']
+        class_names.append(args.label)
+    config.display()
+
+    # Create model
+    model = MaskRCNN(mode="inference", config = config, model_dir = "./")
+
+    # Select weights file to load
+    if args.command == "annotateCoco":
+        weights_path = COCO_WEIGHTS_PATH
+
+        # Download weights file
+        if not os.path.exists(weights_path):
+            utils.download_trained_weights(weights_path)
+    else:
+        weights_path = args.weights
+
+    # Load weights
+    print("Loading weights... ", weights_path)
+    model.load_weights(weights_path, by_name=True)
+
+
+    # Annotate
+    if args.command == "annotateCoco" or args.command == "annotateCustom":
+        annotateImagesInDirectory(model, directory_path=args.image_directory, label = args.label)
+    else:
+        print("'{}' is not recognized. "
+              "Use 'annotateCoco' or 'annotateCustom'".format(args.command))
+
---
+++ customTrain.py
@@ -0,0 +1,255 @@
+import os
+import sys
+import json
+import datetime
+import numpy as np
+import skimage.draw
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../../")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn.config import Config
+from mrcnn import model as modellib, utils
+
+# Path to trained weights file
+COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "./mask_rcnn_coco.h5")
+
+# Directory to save logs and model checkpoints, if not provided
+# through the command line argument --logs
+DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+
+############################################################
+#  Configurations
+############################################################
+
+
+class CustomConfig(Config):
+    """Configuration for training on the toy  dataset.
+    Derives from the base Config class and overrides some values.
+    """
+    # Give the configuration a recognizable name
+    NAME = "CustomLabel"
+
+    IMAGES_PER_GPU = 1
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1  # Background + customLabel
+
+    # Number of training steps per epoch
+    STEPS_PER_EPOCH = 100
+
+    # Skip detections with < 90% confidence
+    DETECTION_MIN_CONFIDENCE = 0.9
+
+
+############################################################
+#  Dataset
+############################################################
+
+class CustomDataset(utils.Dataset):
+
+    def load_custom(self, dataset_dir, subset):
+        """Load a subset of the Custom dataset.
+        dataset_dir: Root directory of the dataset.
+        subset: Subset to load: train or val
+        """
+        # Add classes. We have only one class to add.
+        self.add_class("CustomLabel", 1, "CustomLabel")
+
+        # Train or validation dataset?
+        assert subset in ["train", "val"]
+        dataset_dir = os.path.join(dataset_dir, subset)
+
+        # Load annotations
+        # VGG Image Annotator (up to version 1.6) saves each image in the form:
+        # { 'filename': '28503151_5b5b7ec140_b.jpg',
+        #   'regions': {
+        #       '0': {
+        #           'region_attributes': {},
+        #           'shape_attributes': {
+        #               'all_points_x': [...],
+        #               'all_points_y': [...],
+        #               'name': 'polygon'}},
+        #       ... more regions ...
+        #   },
+        #   'size': 100202
+        # }
+        # We mostly care about the x and y coordinates of each region
+        # Note: In VIA 2.0, regions was changed from a dict to a list.
+        annotations = json.load(open(os.path.join(dataset_dir, "via_region_data.json")))
+        annotations = list(annotations.values())  # don't need the dict keys
+
+        # The VIA tool saves images in the JSON even if they don't have any
+        # annotations. Skip unannotated images.
+        annotations = [a for a in annotations if a['regions']]
+
+        # Add images
+        for a in annotations:
+            # Get the x, y coordinaets of points of the polygons that make up
+            # the outline of each object instance. These are stores in the
+            # shape_attributes (see json format above)
+            # The if condition is needed to support VIA versions 1.x and 2.x.
+            if type(a['regions']) is dict:
+                polygons = [r['shape_attributes'] for r in a['regions'].values()]
+            else:
+                polygons = [r['shape_attributes'] for r in a['regions']] 
+
+            # load_mask() needs the image size to convert polygons to masks.
+            # Unfortunately, VIA doesn't include it in JSON, so we must read
+            # the image. This is only managable since the dataset is tiny.
+            image_path = os.path.join(dataset_dir, a['filename'])
+            image = skimage.io.imread(image_path)
+            height, width = image.shape[:2]
+
+            self.add_image(
+                "customLabel",
+                image_id=a['filename'],  # use file name as a unique image id
+                path=image_path,
+                width=width, height=height,
+                polygons=polygons)
+
+    def load_mask(self, image_id):
+        """Generate instance masks for an image.
+       Returns:
+        masks: A bool array of shape [height, width, instance count] with
+            one mask per instance.
+        class_ids: a 1D array of class IDs of the instance masks.
+        """
+        # If not a custom dataset image, delegate to parent class.
+        image_info = self.image_info[image_id]
+        if image_info["source"] != "customLabel":
+            return super(self.__class__, self).load_mask(image_id)
+
+        # Convert polygons to a bitmap mask of shape
+        # [height, width, instance_count]
+        info = self.image_info[image_id]
+        mask = np.zeros([info["height"], info["width"], len(info["polygons"])],
+                        dtype=np.uint8)
+        for i, p in enumerate(info["polygons"]):
+            # Get indexes of pixels inside the polygon and set them to 1
+            rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
+            mask[rr, cc, i] = 1
+
+        # Return mask, and array of class IDs of each instance. Since we have
+        # one class ID only, we return an array of 1s
+        return mask.astype(np.bool), np.ones([mask.shape[-1]], dtype=np.int32)
+
+    def image_reference(self, image_id):
+        """Return the path of the image."""
+        info = self.image_info[image_id]
+        if info["source"] == "Bird House":
+            return info["path"]
+        else:
+            super(self.__class__, self).image_reference(image_id)
+
+
+def train(model):
+    """Train the model."""
+    # Training dataset.
+    dataset_train = CustomDataset()
+    dataset_train.load_custom(args.dataset, "train")
+    dataset_train.prepare()
+
+    # Validation dataset
+    dataset_val = CustomDataset()
+    dataset_val.load_custom(args.dataset, "val")
+    dataset_val.prepare()
+
+    # *** This training schedule is an example. Update to your needs ***
+    # Since we're using a very small dataset, and starting from
+    # COCO trained weights, we don't need to train too long. Also,
+    # no need to train all layers, just the heads should do it.
+    print("Training network heads")
+    model.train(dataset_train, dataset_val,
+                learning_rate=config.LEARNING_RATE,
+                epochs=30,
+                layers='heads')
+
+
+
+############################################################
+#  Training
+############################################################
+
+if __name__ == '__main__':
+    import argparse
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description='Train Mask R-CNN to detect custom objects.')
+    parser.add_argument("command",
+                        metavar="<command>",
+                        help="'train' or 'splash'")
+    parser.add_argument('--dataset', required=False,
+                        metavar="/path/to/custom/dataset/",
+                        help='Directory of the Custom dataset')
+    parser.add_argument('--weights', required=True,
+                        metavar="/path/to/weights.h5",
+                        help="Path to weights .h5 file or 'coco'")
+    parser.add_argument('--logs', required=False,
+                        default=DEFAULT_LOGS_DIR,
+                        metavar="/path/to/logs/",
+                        help='Logs and checkpoints directory (default=logs/)')
+    parser.add_argument('--image', required=False,
+                        metavar="path or URL to image",
+                        help='Image to apply the color splash effect on')
+    parser.add_argument('--video', required=False,
+                        metavar="path or URL to video",
+                        help='Video to apply the color splash effect on')
+    args = parser.parse_args()
+
+    # Validate arguments
+    if args.command == "train":
+        assert args.dataset, "Argument --dataset is required for training"
+    elif args.command == "splash":
+        assert args.image or args.video,\
+               "Provide --image or --video to apply color splash"
+
+    print("Weights: ", args.weights)
+    print("Dataset: ", args.dataset)
+    print("Logs: ", args.logs)
+
+    # Configurations
+    if args.command == "train":
+        config = CustomConfig()
+
+    # Create model
+    if args.command == "train":
+        model = modellib.MaskRCNN(mode="training", config=config,
+                                  model_dir=args.logs)
+
+
+    # Select weights file to load
+    if args.weights.lower() == "coco":
+        weights_path = COCO_WEIGHTS_PATH
+        # Download weights file
+        if not os.path.exists(weights_path):
+            utils.download_trained_weights(weights_path)
+    elif args.weights.lower() == "last":
+        # Find last trained weights
+        weights_path = model.find_last()
+    elif args.weights.lower() == "imagenet":
+        # Start from ImageNet trained weights
+        weights_path = model.get_imagenet_weights()
+    else:
+        weights_path = args.weights
+
+    # Load weights
+    print("Loading weights ", weights_path)
+    if args.weights.lower() == "coco":
+        # Exclude the last layers because they require a matching
+        # number of classes
+        model.load_weights(weights_path, by_name=True, exclude=[
+            "mrcnn_class_logits", "mrcnn_bbox_fc",
+            "mrcnn_bbox", "mrcnn_mask"])
+    else:
+        model.load_weights(weights_path, by_name=True)
+
+    # Train or evaluate
+    if args.command == "train":
+        train(model)
+    else:
+        print("'{}' is not recognized. "
+              "Use 'train' or 'splash'".format(args.command))
