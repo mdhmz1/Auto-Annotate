eb09d8f6694fc9bb42055de8dfd15a7085b839fd
Muhammad Hamzah <20823685+mdhmz1@users.noreply.github.com>
2021-05-15 23:29:58 +0530

    #11: Label --label does not belong to COCO labels

--- annotate.py
+++ annotate.py
@@ -108,6 +108,21 @@ COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "./mask_rcnn_coco.h5")
 # Directory to save logs, if not provided
 # through the command line argument --logs
 DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+COCO_DATASET_LABELS = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
 
 if __name__ == '__main__':
     import argparse
@@ -138,7 +153,7 @@ if __name__ == '__main__':
 
     # Validate arguments
     if args.command == "annotateCoco":
-        assert args.label in class_names, "Label --label does not belong to COCO labels "
+        assert args.label in COCO_DATASET_LABELS, "Label --label does not belong to COCO labels "
 
     elif args.command == "annotateCustom":
         assert args.label, "Argument --label is required for annotation"
